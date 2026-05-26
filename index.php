93c33ad601881cd6fb64eee124b39e604dfc65da
Kuba9028719 <9028719@student.tcrmbo.nl>
2026-05-26 11:59:30 +0200

    feat(homepage): increase room description font size and update content

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
       <div class="room-card-inner">
         <h2 class="room-title">Space</h2>
         <p class="room-subtitle">Auteur: Kuba</p>
-        <p class="room-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultrices massa vitae dignissim pulvinar. Aenean vestibulum diam sed lectus vehicula, ac venenatis orci congue. Aenean maximus arcu at turpis pretium feugiat. Nam imperdiet dapibus nisl, quis posuere odio vestibulum nec. Donec quis metus ultrices, malesuada nulla vitae, tincidunt turpis. Donec quis convallis magna. Dras iaculis Furvum ullamcorper. Maecenas gravida elementum mi, at mollis nisl ornare sit amet. Ut volutpat lectus nunc, euismod dapibus justo luctus a. Nulla efficitur arcu a ex faucibus, quis auctor dui sagittis. Nunc diam leo, convallis nec mi a dictum convallis metus. Quisque in pulvinar libero. Ut vestibulum malesuada hendrerit.</p>
+        <p class="room-desc">Je bent op een ruimteschip. Het schip heeft een probleem. Je moet de computer repareren voor de tijd om is. Zoek de codes, los de puzzels op en red de bemanning. Heb jij wat nodig is om te overleven in de ruimte?</p>
         <div class="room-play">
           <span class="play-arrow">&#8599;</span> Klik om te spelen!
         </div>
@@ -31,7 +31,7 @@
       <div class="room-card-inner">
         <h2 class="room-title">Middeleeuwen</h2>
         <p class="room-subtitle">Auteur: Armaan</p>
-        <p class="room-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultrices massa vitae dignissim pulvinar. Aenean vestibulum diam sed lectus vehicula, ac venenatis orci congue. Aenean maximus arcu at turpis pretium feugiat. Nam imperdiet dapibus nisl, quis posuere odio vestibulum nec. Donec quis metus ultrices, malesuada nulla vitae, tincidunt turpis. Donec quis convallis magna. Dras iaculis Furvum ullamcorper. Maecenas gravida elementum mi, at mollis nisl ornare sit amet. Ut volutpat lectus nunc, euismod dapibus justo luctus a. Nulla efficitur arcu a ex faucibus, quis auctor dui sagittis. Nunc diam leo, convallis nec mi a dictum convallis metus. Quisque in pulvinar libero. Ut vestibulum malesuada hendrerit.</p>
+        <p class="room-desc">Je zit gevangen in een oud kasteel. De koning heeft je opgesloten. Je moet ontsnappen voor de bewakers terugkomen. Zoek de sleutel, vind de uitgang en ontsnapt uit het kasteel. Lukt het jou om vrij te zijn?</p>
         <div class="room-play">
           <span class="play-arrow">&#8599;</span> Klik om te spelen!
         </div>
