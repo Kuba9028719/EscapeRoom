4b40b73643aa79cb4c55a8dfbed14d8e7ad2f435
Kuba9028719 <9028719@student.tcrmbo.nl>
2026-05-26 11:56:33 +0200

    feat(homepage): add styled room selection page with two escape rooms

--- index.php
+++ index.php
@@ -1,24 +1,44 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="nl">
 
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Escape Room</title>
-  <link rel="stylesheet" href="./css/style.css">
+  <title>EscapeRooms</title>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Jersey+10&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="./css/homepage.css">
 </head>
 
 <body>
+  <h1 class="site-title">EscapeRooms</h1>
 
-  <h1>Welkom</h1>
-  <p>De button hieronder is alleen voor testdoeleinden bij het opstarten van het project. Deze pagina is bedoeld voor de
-    uitleg van jullie escape room.</p>
-  <p>Via deze pagina gaat een team naar de aanmeldpagina</p>
+  <div class="rooms-container">
 
+    <div class="room-card room-card-space">
+      <div class="room-card-inner">
+        <h2 class="room-title">Space</h2>
+        <p class="room-subtitle">Auteur: Kuba</p>
+        <p class="room-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultrices massa vitae dignissim pulvinar. Aenean vestibulum diam sed lectus vehicula, ac venenatis orci congue. Aenean maximus arcu at turpis pretium feugiat. Nam imperdiet dapibus nisl, quis posuere odio vestibulum nec. Donec quis metus ultrices, malesuada nulla vitae, tincidunt turpis. Donec quis convallis magna. Dras iaculis Furvum ullamcorper. Maecenas gravida elementum mi, at mollis nisl ornare sit amet. Ut volutpat lectus nunc, euismod dapibus justo luctus a. Nulla efficitur arcu a ex faucibus, quis auctor dui sagittis. Nunc diam leo, convallis nec mi a dictum convallis metus. Quisque in pulvinar libero. Ut vestibulum malesuada hendrerit.</p>
+        <div class="room-play">
+          <span class="play-arrow">&#8599;</span> Klik om te spelen!
+        </div>
+      </div>
+    </div>
 
-  <button><a href="./rooms/room_1.php">Klik hier voor een demonstratie van kamer
-      1</a></button>
+    <div class="room-card room-card-middeleeuwen">
+      <div class="room-card-inner">
+        <h2 class="room-title">Middeleeuwen</h2>
+        <p class="room-subtitle">Auteur: Armaan</p>
+        <p class="room-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultrices massa vitae dignissim pulvinar. Aenean vestibulum diam sed lectus vehicula, ac venenatis orci congue. Aenean maximus arcu at turpis pretium feugiat. Nam imperdiet dapibus nisl, quis posuere odio vestibulum nec. Donec quis metus ultrices, malesuada nulla vitae, tincidunt turpis. Donec quis convallis magna. Dras iaculis Furvum ullamcorper. Maecenas gravida elementum mi, at mollis nisl ornare sit amet. Ut volutpat lectus nunc, euismod dapibus justo luctus a. Nulla efficitur arcu a ex faucibus, quis auctor dui sagittis. Nunc diam leo, convallis nec mi a dictum convallis metus. Quisque in pulvinar libero. Ut vestibulum malesuada hendrerit.</p>
+        <div class="room-play">
+          <span class="play-arrow">&#8599;</span> Klik om te spelen!
+        </div>
+      </div>
+    </div>
 
+  </div>
 </body>
 
 </html>
